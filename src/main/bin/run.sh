bd4b60ff9e19f913f0ef80cee0e02500bc228e58
mikefinnfkf <mike@fairportkungfu.com>
2021-04-25 22:23:57 -0400

    Initial add

---
+++ src/main/bin/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DATEFMT="+%m/%d/%Y"
+if [ $# -ne 3 ]; then
+  echo "Invalid arguments: create START_DATE END_DATE"
+  echo "Date format: MM/DD/YYYY"
+  exit 1
+fi
+
+CMD=$1
+START_DATE=$2
+END_DATE=$3
+
+DATE=${START_DATE}
+
+while [ ${DATE} != ${END_DATE} ]
+do
+  echo "Processing date ${DATE}"
+  DATE=$(date ${DATEFMT} -d "$DATE + 1 day")
+  java -Djava.util.logging.config.file=./app.logging.properties -jar ./class-manager-0.0.2-SNAPSHOT.jar ${CMD} ${DATE}
+done
+
